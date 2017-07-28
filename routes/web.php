f8487b42cb07a5a092191207edc31dd357a8c8f3
ornab <hzamil17@gmail.com>
2017-07-28 20:21:53 +0600

    Created Resourceful ChannelsController & View files for making Restful CRUD

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,8 @@ Route::get('/{provider}/redirect', [
 		'uses'=>'SocialsController@auth_callback',
 		'as'=> 'social.callback'
 	]);
+
+Route::group(['middleware'=>'auth'], function(){
+
+	Route::resource('channels', 'ChanelsController');
+});
